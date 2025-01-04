b10b50514c5f8e1f95b4e96016c0cea47c3ae332
Ward Smets <81919921+wardgeronimussmets@users.noreply.github.com>
2025-01-04 12:06:51 +0100

    aync request sending

--- python_log_sender/logger.py
+++ python_log_sender/logger.py
@@ -1,7 +1,9 @@
 from colorama import Fore, Style, init
 from datetime import datetime
 from python_log_sender.secrets import get_logger_url
-import requests
+import httpx
+import asyncio
+
 # Initialize colorama
 init()
 
@@ -15,21 +17,28 @@ class TerminalColors:
     
 log_file = "log.txt"
 
-def log(text, color:TerminalColors = TerminalColors.STANDARD):
+async def send_log_async(url, payload):
+    """Asynchronous helper to send log data."""
+    async with httpx.AsyncClient() as client:
+        # send and forget
+        client.post(url, json=payload)
+
+def log(text, color: TerminalColors = TerminalColors.STANDARD):
+    """Logs a message to the console, file, and remote logger."""
     timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     print(f"{timestamp} - {color}{text}{TerminalColors.ENDC}")
+    
     # Append the message to the log file
     try:
         with open(log_file, "a") as file:
             file.write(f"{timestamp} - {text}\n")
     except Exception as e:
         print(f"{TerminalColors.FAIL}Error writing to log file: {e}{TerminalColors.ENDC}")
-    # Send the message to the logger URL
-    try:
-        requests.post(get_logger_url(), json={
-            "timestamp": timestamp, 
-            "logLevel": color, 
-            "message": text
-            })
-    except Exception as e:
-        print(f"{TerminalColors.FAIL}Error sending message to logger: {e}{TerminalColors.ENDC}")
+    
+    # Send the message to the logger URL asynchronously
+    payload = {
+        "timestamp": timestamp,
+        "logLevel": color,
+        "message": text
+    }
+    asyncio.run(send_log_async(get_logger_url(), payload))
